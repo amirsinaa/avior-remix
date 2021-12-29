fa5a0e479ac539e12e2fa61410acbb9f1124e3fe
amirsina <amirsina23@gmail.com>
2021-12-29 12:01:25 +0330

    - boilerplate app skeleton using REMIX command-line tool.

---
+++ app/root.jsx
@@ -0,0 +1,29 @@
+import { Links, Meta, Outlet, ScrollRestoration, LiveReload } from 'remix'
+
+export function meta() {
+	return {
+		title: 'Avior',
+		description: 'Avior',
+	}
+}
+
+export default function App() {
+	return (
+		<html lang='en'>
+			<head>
+				<meta charSet='utf-8' />
+				<meta name='viewport' content='width=device-width,initial-scale=1' />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet" />
+				<Meta />
+				<Links />
+			</head>
+			<body>
+				<Outlet />
+				<ScrollRestoration />
+				{process.env.NODE_ENV === 'development' && <LiveReload />}
+			</body>
+		</html>
+	)
+}
